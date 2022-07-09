865d25e062d4cde6e1f742b08ee864ddb748aaaa
Ajithmc <108991013+Ajithmc@users.noreply.github.com>
2022-07-09 21:13:32 +0530

    Add files via upload

---
+++ Firefox.java
@@ -0,0 +1,26 @@
+package week1.day1;
+
+public class Firefox {
+	String name="Firefox";
+	float version=100.2f;
+	boolean visible=true ;
+	int year=1998 ;
+	char logo='M';
+	public static void main(String[] args) {
+		String name="Firefox";
+		float version=100.2f;
+		boolean visible=true ;
+		int year=1998 ;
+		char logo='M';
+		System.out.println();
+		System.out.println(name);
+		System.out.println(version);
+		System.out.println(visible);
+		System.out.println(year);
+		System.out.println(logo);
+		System.out.println("This is my first script");
+		
+		
+	}
+
+}
